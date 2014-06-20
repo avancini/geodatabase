6b5a919c1f70ff0dd83bc387104f9559bb83dbb0
avancini <ricardo.avancini@yahoo.com.br>
2014-06-20 15:21:20 -0300

    reescrevi as funcoes que geram geojson

--- app.rb
+++ app.rb
@@ -19,44 +19,26 @@ get '/' do
    redirect '/index.html'
 end
 
-
-
-
-## Buscar pontos para uma espécie
+## Busca PONTOS de uma especie
 get '/pontos' do
 
    num=params[:numero]
-   pontos_temp = []
    pontos = []
- 
-   conn.exec("select st_x(geom) as long, st_y(geom) as lat from geo.ocorrencias where id = #{num};") do |result|
+
+   conn.exec("select ST_AsGeoJSON(geom) as pontos from geo.ocorrencias where id = #{num};") do |result|
       result.each do |row|
-         pontos_temp.push({"long" => row['long'], "lat" => row['lat']})  
+      pontos.push(JSON.parse(row['pontos']))
       end
    end
 
-   y = pontos_temp.count - 1
-   
-   # Loop que cria a estrutura do geojson com todos os pontos
-   for x in (0..y) 
-      pontos[x]={
-         :type=>"Feature",
-         :geometry=> {
-             :type=>"Point",
-             :coordinates=>[pontos_temp[x]['long'],pontos_temp[x]['lat']]
-          }
-      }
-   end 
-
-   #puts "pontos ====>  #{pontos}"
+#   puts "PONTOS GeoJSON ====>  #{pontos}"
    content_type :json
    pontos.to_json
 end
 
 
 
-
-
+=begin
 ## Buscar EOO de uma espécie
 get '/eoo' do
 
@@ -98,49 +80,38 @@ get '/eoo' do
    content_type :json
    eoo.to_json
 end
-
-
-
-
-
-## Buscar subpopulacoes de uma espécie
-get '/subpopulacoes' do
+=end
+## Busca EOO de uma especie
+get '/eoo' do
 
    num=params[:numero]
-   subpopulacoes_temp = []
-   subpopulacoes = []
+   eoo = []
 
-   conn3.exec("select st_astext(geom) as poligono from geo.subpopulacoes where id = #{num};") do |result3|
-      result3.each do |row3|
-         txt3= row3['poligono'].gsub("POLYGON((","").gsub("))","")
-         #puts "txt = #{txt}"
-         vertices_sub=txt3.split(",")
-         #puts "vertices = #{vertices}"
-         poli_sub=[]
-         for i in (0..vertices_sub.count-1)
-            vertice_sub = vertices_sub[i].split(" ")
-            #puts "vertice =#{vertice}"
-            vertice_sub[0] = vertice_sub[0].to_f
-            vertice_sub[1] = vertice_sub[1].to_f
-            #puts "vertice =#{vertice}"
-            poli_sub.push(vertice_sub)
-         end
-         subpopulacoes_temp.push(poli_sub)
+   conn2.exec(" select st_asgeojson(geom) as poligono from geo.eoo where id = #{num};") do |result2|
+      result2.each do |row2|
+      eoo.push(JSON.parse(row2['poligono']))
       end
    end
 
-   # loop que cria a estrutura do geojson com o poligono de subpopulacoes
-   for x in (0..subpopulacoes_temp.count-1)
-      subpopulacoes[x]={
-         :type=>"Feature",
-         :geometry=> {
-             :type=>"Polygon",
-             :coordinates=>[subpopulacoes_temp[x]]
-          }
-      }
+   puts "EOO GeoJSON ====>  #{eoo}"
+   content_type :json
+   eoo.to_json
+end
+
+
+## Busca SUBPOPULACOES de uma especie
+get '/subpopulacoes' do
+
+   num=params[:numero]
+   subpopulacoes = []
+
+   conn3.exec("select ST_AsGeoJSON(geom) as poligono from geo.subpopulacoes where id = #{num};") do |result3|
+      result3.each do |row3|
+      subpopulacoes.push(JSON.parse(row3['poligono']))
+      end
    end
 
-   #puts "Subpopulacoes ====>  #{subpopulacoes}"
+#   puts "SUBPOPULACOES GeoJSON ====>  #{subpopulacoes}"
    content_type :json
    subpopulacoes.to_json
 end
@@ -150,90 +121,35 @@ end
 get '/aoo' do
 
    num=params[:numero]
-   aoo_temp = []
    aoo = []
 
-   conn4.exec("select st_astext(geom) as poligono from geo.aoo where id = #{num};") do |result4|
+   conn4.exec("select ST_AsGeoJSON(geom) as poligono from geo.aoo where id = #{num};") do |result4|
       result4.each do |row4|
-         txt4= row4['poligono'].gsub("POLYGON((","").gsub("))","")
-         #puts "txt = #{txt}"
-         vertices_aoo=txt4.split(",")
-         #puts "vertices = #{vertices}"
-         poli_aoo=[]
-         for i in (0..vertices_aoo.count-1)
-            vertice_aoo = vertices_aoo[i].split(" ")
-            #puts "vertice =#{vertice}"
-            vertice_aoo[0] = vertice_aoo[0].to_f
-            vertice_aoo[1] = vertice_aoo[1].to_f
-            #puts "vertice =#{vertice}"
-            poli_aoo.push(vertice_aoo)
-         end
-         aoo_temp.push(poli_aoo)
+      aoo.push(JSON.parse(row4['poligono']))
       end
    end
 
-   # loop que cria a estrutura do geojson com o poligono de AOO
-   for x in (0..aoo_temp.count-1)
-      aoo[x]={
-         :type=>"Feature",
-         :geometry=> {
-             :type=>"Polygon",
-             :coordinates=>[aoo_temp[x]]
-          }
-      }
-   end
-
-   puts "AOO ====>  #{aoo}"
+#   puts "AOO GeoJSON ====>  #{aoo}"
    content_type :json
    aoo.to_json
 end
 
 
-
-## Busca remanescentes de uma especie
+## Busca REMANESCENTES de uma especie
 get '/remanescentes' do
 
    num=params[:numero]
-   remanescentes_temp = []
    remanescentes = []
 
-   conn5.exec("select st_astext(geom) as poligono from geo.remanescentes where st_intersects(geom, (select st_union(geom) from geo.subpopulacoes where id = #{num})) and legenda = 'Mata';") do |result5|
+   conn5.exec("select ST_AsGeoJSON(geom) as poligono from geo.remanescentes where st_intersects(geom, (select st_union(geom) from geo.subpopulacoes where id = #{num})) and legenda = 'Mata';") do |result5|
       result5.each do |row5|
-         txt5= row5['poligono'].gsub("POLYGON((","").gsub("))","")
-         puts "txt5 =====>>>>>>>> #{txt5}"
-         puts "<<<<<<<<========= txt5"
-         vertices_remanescentes=txt5.split(",")
-         #puts "vertices = #{vertices}"
-         poli_remanescentes=[]
-         for i in (0..vertices_remanescentes.count-1)
-            vertice_remanescentes = vertices_remanescentes[i].split(" ")
-            #puts "vertice =#{vertice}"
-            vertice_remanescentes[0] = vertice_remanescentes[0].to_f
-            vertice_remanescentes[1] = vertice_remanescentes[1].to_f
-            poli_remanescentes.push(vertice_remanescentes)
-         end
-         remanescentes_temp.push(poli_remanescentes)
+      remanescentes.push(JSON.parse(row5['poligono']))
       end
    end
 
-   # loop que cria a estrutura do geojson com o poligono de remanescentes 
-   for x in (0..remanescentes_temp.count-1)
-#puts remanescentes_temp[x]
-      remanescentes[x]={
-         :type=>"Feature",
-         :geometry=> {
-             :type=>"Polygon",
-             :coordinates=>[remanescentes_temp[x]]
-          }
-      }
-   end
-
-   #puts "remanescentes ====>  #{remanescentes}"
+#   puts "REMANESCENTES GeoJSON ====>  #{remanescentes}"
    content_type :json
    remanescentes.to_json
 end
-
-
-
 
 
